37a309fc1f5fb1674aaad75bc7db769da1feba71
Raphael Karanja <rkaranja@cytonn.com>
2018-12-03 13:56:17 +0300

    create first react component 'HOME'

---
+++ src/components/homePage.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Raphael Karanja on 03/12/2018.
+ */
+"use strict";
+import React, {Component} from 'react';
+class Home extends Component {
+    render(){
+        return (
+           <div>
+               
+            </div>
+        );
+    }
+}
