c3c30754dc14befe61488b657ab2f12e6ed4794a
Raphael Karanja <rkaranja@cytonn.com>
2018-12-03 10:37:43 +0300

    add gulp configuration to compile JS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 let gulp = require('gulp');
 let connect = require('gulp-connect'); //Runs local dev server
 let open = require('gulp-open'); //Opens a Url in web browser
+let browserify = require('browserify'); //Bundle JavaScript
+let reactify = require('reactify'); //Transforms React JSX to JS
+let source = require('vinyl-source-stream'); //Use conventional text stream with Gulp
 
 //config
 let config = {
@@ -9,7 +12,9 @@ let config = {
     devBaseUrl: 'http:localhost',
     paths: {
         html: './src/*.html',
-        dist: './dist'
+        js: './src/**/*.js',
+        dist: './dist',
+        mainJs: './src/main.js'
     }
 };
 //start local development server
@@ -33,8 +38,20 @@ gulp.task('html', ()=>{
         .pipe(connect.reload());
 });
 
+gulp.task('js', ()=>{
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/js'))
+        .pipe(connect.reload())
+
+});
+
 gulp.task('watch', ()=>{
     gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
 });
-gulp.task('default', ['html', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch']);
 
---
+++ src/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Raphael Karanja on 03/12/2018.
+ */
+let App = console.log('Hello I am a demo app');
+
+module.exports = App;
